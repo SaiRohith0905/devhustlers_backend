8340b7e6801ac175bb4e8b55f84294066965beab
saiRohith <saiRohith0905@gmail.com>
2025-03-19 01:26:17 +0530

    database-connection-established-made-first-apicall-for-the-app

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 const express = require("express");
-
+const {
+  connectToMongoDBUsingMongoose,
+} = require("./src/config/mongoose.config.js");
 const app = express();
 
+const UserModel = require("./src/models/UserModel.js");
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Helloo from server");
+  res.send("Hello from server");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("This another hello from hello path");
+app.post("/user/signUp", async (req, res) => {
+  //creating a new instance of usermodel
+  const { firstName, lastName, emailId, password, age, gender } = req.body;
+
+  const newUser = UserModel({
+    firstName: firstName,
+    lastName: lastName,
+    emailId: emailId,
+    password: password,
+    age: age,
+    gender: gender,
+  });
+
+  try {
+    await newUser.save();
+    res.status(200).send("User SignedUp Successfully, new user:" + newUser);
+  } catch (error) {
+    res.status(400).send("Something went wrong:" + error.message);
+  }
 });
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Application error !! Something went wrong");
+  }
 });
+
+connectToMongoDBUsingMongoose()
+  .then(() => {
+    console.log("Successfully connected to mongodb using mongoose");
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("failed to connect to db");
+  });
